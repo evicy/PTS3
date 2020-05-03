a5170b44e39b963a17574de03b35636c761b0a43
evi <herencsareva@gmail.com>
2020-05-03 13:32:40 +0200

    Added seqential version of complete_neighbourhood

--- initialize_nodes.py
+++ initialize_nodes.py
@@ -4,6 +4,25 @@ from threading import Thread
 import time
 import requests
 
+def complete_neighbourhood(start,  HOST = "localhost"):
+    r = requests.get(f'http://{HOST}:{start}/')
+    neighbours = r.text.split(',')
+    neighbours.append(start)
+    print(neighbours)
+
+    def add_edge(e_from, e_to):
+        r = requests.get(f'http://{HOST}:{e_from}/new?port={e_to}')
+
+    for x in neighbours:
+        for y in neighbours:
+            if x != y:
+                add_edge(x,y)
+                #print("adding from", x, " to ", y)
+
+    print("complete_neighbourhood ready")
+
+
+
 
 #The condition argument is for you to know when everithing is running
 def do_stuff(HOST, nodes, edges, condition_ready=None, condition_done=None):
@@ -20,7 +39,9 @@ def do_stuff(HOST, nodes, edges, condition_ready=None, condition_done=None):
         for x,y in edges:
             add(x,y)
             add(y, x)
-    
+
+        complete_neighbourhood(8031, HOST)
+
         #This is here for you so you know when stuff is ready
         if condition_ready is not None:
             with condition_ready:
@@ -31,7 +52,7 @@ def do_stuff(HOST, nodes, edges, condition_ready=None, condition_done=None):
                 condition_done.wait()
         else:
             while True:
-                time.sleep(5)
+                time.sleep(0.5)
 
     except KeyboardInterrupt:
         pass
@@ -46,7 +67,9 @@ if __name__=="__main__":
     HOST = "localhost"
     graph_base = 8030
     graph = {(0,1), (1, 2), (1, 3), (1, 4), (3, 4), (4, 5)}
+    graph = {(0,1), (1,2), (5,4)}
     graph = {(graph_base+x, graph_base+y) for x,y in graph}
     nodes = {x for y in graph for x in y}
     do_stuff(HOST, nodes, graph)
+    print("haloo")
 
