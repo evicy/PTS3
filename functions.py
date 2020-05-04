6508502571235e4451984d67c506711d09b63faf
evi <herencsareva@gmail.com>
2020-05-05 00:21:04 +0200

    Reimplemented functions - now they are async

---
+++ functions.py
@@ -0,0 +1,86 @@
+import requests
+import asyncio
+
+
+HOST = "localhost"
+
+async def get_list_of_neighbours(start,  HOST = "localhost"):
+    try:
+        r = requests.get(f'http://{HOST}:{start}/')
+        neighbours = r.text.split(',')
+        neighbours = list(map(int, neighbours))
+        return neighbours
+    except:
+        return []
+
+async def add_edge(e_from, e_to):
+   r = requests.get(f'http://{HOST}:{e_from}/new?port={e_to}')
+   print("adding edge from ", e_from, "  to ", e_to)
+
+async def complete_neighbourhood(start,  HOST = "localhost"):
+   print("complete_neighbourhood, start node = ", start)
+   neighbours = await get_list_of_neighbours(start, HOST)
+
+   neighbours.append(start)
+   print(neighbours)
+
+   tasks = []
+   for x in neighbours:
+       for y in neighbours:
+          if x != y:
+               tasks.append(asyncio.create_task(add_edge(x,y)))
+
+   await asyncio.gather(*tasks)
+   print("complete_neighbourhood ready")
+
+
+async def climb_degree(start, HOST="localhost"):
+    print("climb_degree, start node = ", start)
+    neighbours = await get_list_of_neighbours(start, HOST)
+    max_degree = len(neighbours)
+    if max_degree == 0:
+        return start
+
+    port_with_max_degree = start
+
+    async def get_degrees(node):
+        node_neighbours = await get_list_of_neighbours(node, HOST)
+        return len(node_neighbours)
+
+    tasks = [asyncio.create_task(get_degrees(node)) for node in neighbours]
+    tasks_results = await asyncio.gather(*tasks)
+    print("tasks_result = ", tasks_results)
+
+    for i in range(len(tasks_results)):
+        if (tasks_results[i] > max_degree) or (tasks_results[i] == max_degree and port_with_max_degree > neighbours[i]):
+            max_degree = tasks_results[i]
+            port_with_max_degree = neighbours[i]
+
+    if start != port_with_max_degree:
+        return await climb_degree(port_with_max_degree)
+
+    return port_with_max_degree
+
+
+async def distance4(start):
+    actual_nodes = set()
+    actual_nodes.add(start)
+    visited_nodes = set()
+    visited_nodes.add(start)
+
+    for i in range(4):
+        visited_nodes.update(actual_nodes)
+
+        tasks = [asyncio.create_task(get_list_of_neighbours(node)) for node in actual_nodes]
+        tasks_results = await asyncio.gather(*tasks)
+
+        new_neighbours = set()
+        for l in tasks_results:
+            new_neighbours.update(l)
+
+        for n in visited_nodes:
+            if n in new_neighbours:
+                new_neighbours.remove(n)
+        actual_nodes = new_neighbours
+
+    return actual_nodes
--- initialize_nodes.py
+++ initialize_nodes.py
@@ -3,75 +3,11 @@ from node import get_handler
 from threading import Thread
 import time
 import requests
-
-def get_list_of_neighbours(start,  HOST = "localhost"):
-    r = requests.get(f'http://{HOST}:{start}/')
-    neighbours = r.text.split(',')
-    neighbours = list(map(int, neighbours))
-    return neighbours
-
-def complete_neighbourhood(start,  HOST = "localhost"):
-    neighbours = get_list_of_neighbours(start, HOST)
-
-    neighbours.append(start)
-    print(neighbours)
-
-    def add_edge(e_from, e_to):
-        r = requests.get(f'http://{HOST}:{e_from}/new?port={e_to}')
-
-    for x in neighbours:
-        for y in neighbours:
-            if x != y:
-                add_edge(x,y)
-                #print("adding from", x, " to ", y)
-
-    print("complete_neighbourhood ready")
-
-
-def climb_degree(start, HOST = "localhost"):
-    neighbours = get_list_of_neighbours(start, HOST)
-    max_degree = len(neighbours)
-    if max_degree == 0:
-        return start
-
-    port_with_max_degree = start
-
-    degrees = []
-    for n in neighbours:
-        degrees.append((n, len(get_list_of_neighbours(n, HOST))))
-
-    print(degrees)
-    degrees = sorted(degrees, key=lambda element: (element[1] * -1, element[0]))
-    print(degrees)
-
-    best_tuple = degrees[0]
-
-    if best_tuple[1] == max_degree:
-        return min(best_tuple[0], port_with_max_degree)
-
-    if best_tuple[1] > max_degree:
-        return climb_degree(best_tuple[0], HOST)
-
-    return port_with_max_degree
-
-
-def distance4(start, result, visited_sooner, distance=0):
-    visited_sooner.add(start)
-    neighbours = get_list_of_neighbours(start, HOST)
-
-    if distance == 3:
-        for n in neighbours:
-            if n not in visited_sooner:
-                result.add(n)
-
-    if distance < 3:
-        for n in neighbours:
-            distance4(n, result, visited_sooner, distance+1)
-
-
+from functions import *
+import asyncio
 
 #The condition argument is for you to know when everithing is running
-def do_stuff(HOST, nodes, edges, condition_ready=None, condition_done=None):
+async def do_stuff(HOST, nodes, edges, condition_ready=None, condition_done=None):
     servers = list(HTTPServer((HOST, port), get_handler()) for port in nodes)
 
     try:   
@@ -88,10 +24,19 @@ def do_stuff(HOST, nodes, edges, condition_ready=None, condition_done=None):
 
         #complete_neighbourhood(8031, HOST)
         #print("port with highest degree:", climb_degree(8030,HOST))
-        result = set()
-        visited = set()
-        distance4(8030, result, visited)
-        print(result.difference(visited))
+        #result = set()
+        #visited = set()
+        #distance4(8030, result, visited)
+        #print(result.difference(visited))
+
+        #x = await complete_neighbourhood(8030)
+        #print(x)
+
+        #max_port = await climb_degree(8030)
+        #print(max_port)
+
+        result = await distance4(8030)
+        print(result)
 
         #This is here for you so you know when stuff is ready
         if condition_ready is not None:
@@ -114,13 +59,13 @@ def do_stuff(HOST, nodes, edges, condition_ready=None, condition_done=None):
     for t in threads: t.join()
 
 
-if __name__=="__main__":
+async def main():
     HOST = "localhost"
     graph_base = 8030
     graph = {(0,1), (1, 2), (1, 3), (1, 4), (3, 4), (4, 5)}
 
     # just a shorter example
-    #graph = {(0,1), (1,2), (5,4)}
+    graph = {(0,1), (1,2), (5,4)}
 
     #for climb_degree
     #graph = {(0,4), (0,2), (2,0), (2,1), (2,3), (4,0), (4,1), (4,3), (3,0), (3,1), (3,2), (3,4)}
@@ -130,6 +75,12 @@ if __name__=="__main__":
 
     graph = {(graph_base+x, graph_base+y) for x,y in graph}
     nodes = {x for y in graph for x in y}
-    do_stuff(HOST, nodes, graph)
+    await do_stuff(HOST, nodes, graph)
     print("haloo")
+    #x = await complete_neighbourhood(8030)
+    #print(x)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
